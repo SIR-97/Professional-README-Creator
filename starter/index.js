3630d59ab7b891e0ac4383818eb6d354b73f9b54
SIR-97 <sirafiq.1997@gmail.com>
2023-03-06 19:00:39 +0000

    created my initial questions for the command line based on the acceptance criterior

--- starter/index.js
+++ starter/index.js
@@ -5,6 +5,52 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is your project's title?",
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a brief description of your project:',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter installation instructions for your project:',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter usage instructions for your project:',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please enter contribution guidelines for your project:',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Please enter test instructions for your project:',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose a license for your project:',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      },
 
 ];
 
